590019dc5c55b8b05e07543fdbca082e08c1f815
Barbara Perlik <barbperlik@gmail.com>
2018-02-12 23:57:21 -0500

    Chkpt 6 completed

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -53,6 +53,10 @@ module Persistence
     self.class.update(self.id, updates)
   end
 
+  def destroy
+    self.class.destroy(self.id)
+  end
+
   # use method_missing to add support for dynamic update_*
   # and update_attributes
   def method_missing(method_name, * arguments, &block)
@@ -128,6 +132,38 @@ module Persistence
 
     def update_all(updates)
       update(nil, updates)
+    end
+
+    def destroy(*id)
+      if id.length > 1
+        where_clause = "WHERE id IN (#{id.join(",")});"
+      else
+        where_clause = "WHERE id = #{id.first};"
+      end
+
+      connection.execute <<-SQL
+        DELETE FROM #{table} #{where_clause}
+      SQL
+
+      true
+    end
+
+    def destroy_all(conditions_hash=nil)
+      if conditions_hash && !conditions_hash.empty?
+        conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+        conditions = conditions_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+          WHERE #{conditions};
+        SQL
+      else
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+        SQL
+      end
+
+      true
     end
   end
 end
